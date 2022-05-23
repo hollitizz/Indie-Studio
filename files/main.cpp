bfdfc52bf8f6a5f30bcf5499fefede878fdd332c
Matteo Linzi <matteo.linzi@epitech.eu>
2022-05-23 12:00:21 +0200

    [add] basic cmake

---
+++ files/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** Untitled (Workspace)
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    std::cout << "ok" << std::endl;
+    return 0;
+}
