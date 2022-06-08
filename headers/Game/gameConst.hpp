7d9610fc5ed35e5ac654f9b6a80cb93c14552a7c
Valentin Caure <valentin.caure@epitech.eu>
2022-06-08 12:41:32 +0200

    [ADD] keymaps

--- headers/Game/gameConst.hpp
+++ headers/Game/gameConst.hpp
@@ -17,7 +17,7 @@ const std::array<Vector2, 4> PLAYER_STARTS_POSITION = {
 
 const std::array<std::array<KeyboardKey, 5>, 4> PLAYER_KEY_MAP = {
     std::array<KeyboardKey, 5> {KEY_Q, KEY_W, KEY_S, KEY_A, KEY_D},
-    std::array<KeyboardKey, 5> {KEY_M, KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT},
-    std::array<KeyboardKey, 5> {KEY_M, KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT},
-    std::array<KeyboardKey, 5> {KEY_M, KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT}
+    std::array<KeyboardKey, 5> {KEY_R, KEY_T, KEY_G, KEY_F, KEY_H},
+    std::array<KeyboardKey, 5> {KEY_SLASH, KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT},
+    std::array<KeyboardKey, 5> {KEY_U, KEY_I, KEY_K, KEY_J, KEY_L}
 };
